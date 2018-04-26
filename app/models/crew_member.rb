a76b0bf8eeb09ac69b24b1c41e74e083ab7fcdb4
Yenson Tam <yensontam@Yensons-MBP.home>
2018-04-26 05:21:56 -0400

    fix associations

--- app/models/crew_member.rb
+++ app/models/crew_member.rb
@@ -2,7 +2,6 @@ class CrewMember < ActiveRecord::Base
 
   belongs_to :person
   belongs_to :work
-  has_many :crew_members_roles
-  has_many :roles, through: :crew_members_roles
+  has_many :roles
 
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ActiveRecord::Base
 
   has_many :crew_members
   has_many :works, through: :crew_members
-  has_and_belongs_to_many :crew_members_roles, through: :crew_members
+  has_many :roles, through: :crew_members
 
   validates :name, presence: true
   validates :photo_url, format: URI::regexp(%w(http https)), allow_nil: true
