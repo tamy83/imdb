cd60abbe3db227cb5d906f3f312218661adfd571
Yenson Tam <yensontam@Yensons-MBP.home>
2018-04-27 11:05:11 -0400

    now filters by mm and dd

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,26 +7,15 @@ class Person < ActiveRecord::Base
   validates :name, presence: true
   validates :photo_url, format: URI::regexp(%w(http https)), allow_nil: true
   validates :profile_url, format: URI::regexp(%w(http https)), allow_nil: true
-#  validates :work_ranking_valid?, if: :work_ranking 
 
+  #todo: add column known_for_ranking, comma delimited string of work_ids that corresponds users known for works
 
   def self.find_by_birth_month_day_and_role(month, day, role)
     # todo: validate month and day
-    Person.all.map(&:to_h) 
+    Person.where('extract(month from birthdate) = ? AND extract(day from birthdate) = ?', month.to_i, day.to_i).map(&:to_h)
   end
 
   def to_h
-=begin
-{ name: "Gemma Arterton", 
-	photoUrl: "https://images-na.ssl-images-amazon.com/images/M/MV5BOTAwNTMwMzE5OF5BMl5BanBnXkFtZTgwMjYwNzI2MjE@._V1_UX140_CR0,0,140,209_AL_.jpg", 
-	profileUrl: "http://www.imdb.com/name/nm2605345", 
-	mostKnownWork: 
-	{ title: "Prince of Persia: The Sands of Time", 
-	url: "http://www.imdb.com/title/tt0473075/", 
-	rating: 6.6, 
-	director: "Louis Leterrier" } 
-	}, ... ] } 
-=end
     most_known_work = !works.empty? ? works.first.to_h : {}
     return { name: name, photoUrl: photo_url, profileUrl: profile_url, mostKnownWork: most_known_work }
   end
